4b20dd01d32b786f9c7d25dbef83e0f9a3006ff8
Richard Yvarra <richard.yvarra@gmail.com>
2021-04-07 16:56:11 -0400

    commit js application

---
+++ index.js
@@ -0,0 +1,37 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require ("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require ("./lib/htmlRenderer");
+
+let teamMembers = [];
+
+const firstQuestion = {
+ type: "list",
+ message = "Would you like to add a member or generate current team?",
+ name= "Add",
+ choices = ["Add Member", "Generate Team"],
+
+
+};
+
+const yourRole = {
+   type: "list",
+   message = "What member do you want to Add?",
+   name = "Role",
+   choices = ["Intern", "Engineer", "Manager"],
+};
+
+const fileQuestion = {
+    type: "input",
+    message: "Please enter a valid name",
+    name: "fileName",
+
+
+};
